ff01c5557c12951e6273934cd40edc7534811dd9
Daniel <tonystar.dm@gmail.com>
2021-12-12 21:39:58 -0600

    Segundo jugador solo posicion

--- game.js
+++ game.js
@@ -1,9 +1,16 @@
 //La escenas deben ir arriba de la variable de configurcion
 
 /*variable para controlar el sprite del Personaje1*/
-var jugador;
+var jugador1
+var jugador2
+
+//
+var secondPlayer = false //Bandera si es que existe un segundo jugador
+
 /*Variables para agregar movimiento al jugador*/
 var arriba, abajo, izquierda, derecha;
+var secondX; //Posicion en x del segundo jugador
+var secondY; //Posicion en y del segundo jugador
 const velocidad = 350;
 const alturaSalto = -530;
 
@@ -48,20 +55,22 @@ var EscenaJuego = new Phaser.Class({
         },
         
         preload() {
+            
+            
             /*importamos el sprite del primer personaje */
             this.load.spritesheet('personaje1', 'assets/sprites/personaje1.png', { frameWidth: 57, frameHeight: 62 });
-        
+            /*if(secondPlayer!=false){
+                this.load.spritesheet('personaje1', 'assets/sprites/personaje1.png', { frameWidth: 57, frameHeight: 62 });
+            }*/
             // Importamos el JSON para generar nuestro mapa
             this.load.tilemapTiledJSON('mapa', 'assets/mapa/mapa.json')
             this.load.image('tiles', 'assets/mapa/tileSets.png')
         },
         /*Inicializamos el sprite del personaje1*/
         create() {
-            // Al escuchar el evento jugador-saltando 
-            socket.on('jugador-saltando', () => {
-                console.log("Algun jugador esta saltando")
-            })
-        
+         
+           
+            
             game.config.backgroundColor.setTo(100, 210, 222) //RGB
         
             // Creamos el objeto mapa con sus tilesets
@@ -76,10 +85,17 @@ var EscenaJuego = new Phaser.Class({
             solidos.setCollisionByProperty({ solido: true }) //Agreando la propiedad de solido a los solidos
         
             /*con physics.add agregamos fisicas al sprite*/
-            jugador = this.physics.add.sprite(100, 100, 'personaje1', 0);//sprite(posicion en x,y,nombreDelSprite,numeroSprite)
+            jugador1 = this.physics.add.sprite(100, 100, 'personaje1', 0);//sprite(posicion en x,y,nombreDelSprite,numeroSprite)
             // jugador.setCollideWorldBounds(true);//Hacer que choque con los limites del mundo
-            jugador.setSize(30, 0) // Para cambiar el hitbox del jugador
-        
+            jugador1.setSize(30, 0) // Para cambiar el hitbox del jugador
+            
+            
+             /*con physics.add agregamos fisicas al sprite*/
+            jugador2 = this.physics.add.sprite(100, 100, 'personaje1', 0);//sprite(posicion en x,y,nombreDelSprite,numeroSprite)
+            //jugador2.setCollideWorldBounds(true);//Hacer que choque con los limites del mundo
+            jugador2.setVisible(false);
+            this.physics.add.collider(jugador2, solidos)
+            jugador2.setSize(30, 0) // Para cambiar el hitbox del jugador
         
             //creamos la animacion de caminar para el spirte personaje1
             this.anims.create({
@@ -88,11 +104,11 @@ var EscenaJuego = new Phaser.Class({
                 frameRate: 10
             });
         
-            this.physics.add.collider(jugador, solidos) //Ahora el jugador colisiona con los objetos con propiedad solidos
+            this.physics.add.collider(jugador1, solidos) //Ahora el jugador colisiona con los objetos con propiedad solidos
         
             // Estas lineas sirven para que la camara se centre en el jugador
             this.cameras.main.setBounds(0, 0, mapa.widthInPixels, mapa.healthInPixels)
-            this.cameras.main.startFollow(jugador)
+            this.cameras.main.startFollow(jugador1)
         
             /*asignamos las entradas del teclado con las que se movera el jugador*/
             arriba = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
@@ -100,33 +116,46 @@ var EscenaJuego = new Phaser.Class({
             derecha = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
         },
         update() {
+           
             //SIEMPRE inicializar la velocidad del jugador en 0
-            jugador.body.setVelocityX(0);
-        
+            jugador1.body.setVelocityX(0);
+            
+                var posP1 = {
+                    x: jugador1.x,
+                    y: jugador1.y
+                }
+                socket.emit('posicion',posP1)
+
+            jugador2.x = secondX;
+            jugador2.y = secondY;
             //Habilitamos las teclas del teclado con las que el jugador se movera
             if (izquierda.isDown) {
-                jugador.body.setVelocityX(-velocidad);//se le resta en x para que vaya a la izq
-                jugador.flipX = true;//para que se volte el sprite del jugador cuando camina en direccion contraria
+                jugador1.body.setVelocityX(-velocidad);//se le resta en x para que vaya a la izq
+                jugador1.flipX = true;//para que se volte el sprite del jugador cuando camina en direccion contraria
             }
         
             if (derecha.isDown) {
-                jugador.body.setVelocityX(velocidad); //se le suma en x para que vaya a la derecha
-                jugador.flipX = false;//para que se volte el sprite del jugador cuando camina en direccion contraria
+                jugador1.body.setVelocityX(velocidad); //se le suma en x para que vaya a la derecha
+                jugador1.flipX = false;//para que se volte el sprite del jugador cuando camina en direccion contraria
             }
         
-            if (arriba.isDown && jugador.body.onFloor()) {//onFloor() hacer que solo se pueda saltar cuando se esta tocando el suelo
-                jugador.body.setVelocityY(alturaSalto);
-                socket.emit('saltar') // Para comunicarnos con los otros jugadores clientes del socket
+            if (arriba.isDown && jugador1.body.onFloor()) {//onFloor() hacer que solo se pueda saltar cuando se esta tocando el suelo
+                jugador1.body.setVelocityY(alturaSalto);
+                //socket.emit('saltar') // Para comunicarnos con los otros jugadores clientes del socket
             }
-        
+            if(secondPlayer){               //Aqui es donde se actualiza la posicion del jugador2
+                jugador2.setVisible(true);
+                secondPlayer.x = secondX;
+                secondPlayer.y = secondY; 
+            }
             //Para activar la animacion de caminar
-            if ((izquierda.isDown || derecha.isDown) && jugador.body.onFloor()) {
-                jugador.anims.play('caminar', true);//el jugador esta caminando
-            } else if (!jugador.body.onFloor()) {//si el jugador NO esta en el suelo
-                jugador.setFrame(9);//el jugador esta cayendo
+            if ((izquierda.isDown || derecha.isDown) && jugador1.body.onFloor()) {
+                jugador1.anims.play('caminar', true);//el jugador esta caminando
+            } else if (!jugador1.body.onFloor()) {//si el jugador NO esta en el suelo
+                jugador1.setFrame(9);//el jugador esta cayendo
             } else {
-                jugador.setFrame(0); //el jugador esta quieto
-            }
+                jugador1.setFrame(0); //el jugador esta quieto
+            } 
         }
 
 });
@@ -148,3 +177,19 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+
+socket.on('readyPlayer', (data)=> { //Conexion con el numero de jugadores que tiene
+    console.log("Jugadores = "+data)
+    if(data==2){
+      secondPlayer = true;  
+    }
+    
+}) 
+socket.on('jugador-saltando', () => { 
+    console.log("Algun jugador esta saltando")
+}) 
+/socket.on('posPlayer2',(data)=>{ //Conexion socket con la posicion del segundo jugador
+    console.log("El player dos envio estos datos:  " + data.x + data.y  );
+    secondX=data.x;
+    secondY=data.y;
+})
--- socket/socket.js
+++ socket/socket.js
@@ -1,20 +1,52 @@
 let app = require("express")()
 let http = require("http").Server(app)
-let io = require("socket.io")(http)
+let io = require('socket.io')(http)
+
+
+var connections = 0;
+var posPlayer1;
+var one = false;
+var two = false;
+var dataId;
+let readyPlayer;
+
 
 // En cada nueva connecion
 io.on('connection',(socket) => {
     console.log("Nueva conexion")
+    var connectedUsersCount = io.engine.clientsCount; //Numero de usuarios conectados
+    console.log("Numero de usarios" + connectedUsersCount)
+   
+    socket.emit('readyPlayer',connectedUsersCount);//envia a todos los players dependiendo de cuántos players están conectados s
+    
+    
+    var escucha = escuchaPlayers(socket);
 
     socket.on('saltar', () => {
-        socket.broadcast.emit('jugador-saltando') // Si un socket envio el evento saltar, 
+        socket.emit('jugador-saltando')
         // entonces el socket hace un boradcast para anunciarlo a todos los clientes
     })
+    socket.on('posicion',(data)=>{
+        console.log("Me estan enviado la posicion");
+        console.log(data.x + "   "+data.y);
+        socket.broadcast.emit('posPlayer2',data);
+    });
 })
 
-var port = 2525
+
+
+var port = 5500
 
 // La aplicacion escucha en el puerto port
 http.listen(port, function() {
     console.log("listening in http://localhost:"+port)
 })
+
+function escuchaPlayers(socket){
+
+    socket.on('posPlayer1',(data)=>{
+        socket.broadcast.emit('posPlayer2',data)
+    })
+
+  }
+  
